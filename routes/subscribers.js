f043dc4f994eba9e1173d4c49d9505d448420813
code-reaper08 <v.i.s.h.w.a.08.11.2001@gmail.com>
2021-06-28 23:38:15 +0530

    added get, post, patch and delete routes

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,4 +1,34 @@
 const express = require('express')
 const router = express.Router()
 
+// get
+router.get('/', (req,res) => {
+
+})
+
+// getting one
+router.get('/:id', (req,res) => {
+
+})
+
+// creating a user
+router.get('/', (req,res) => {
+
+})
+
+// Update one
+router.patch('/', (req,res) => {
+
+})
+
+// delete one
+router.delete('/:id', (req,res) => {
+
+})
+
+
+
+
+
+
 module.exports = router
