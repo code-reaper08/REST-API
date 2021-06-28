0549373571cbbb5ea50df9c8db0879d0b2003412
code-reaper08 <v.i.s.h.w.a.08.11.2001@gmail.com>
2021-06-28 23:54:50 +0530

    Created schema and model for the DB

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const Subscriber = require("../models/subscriber")
 
 // get
-router.get('/', (req,res) => {
+router.get('/', async (req,res) => {
     res.send('Hello from main')
 })
 
