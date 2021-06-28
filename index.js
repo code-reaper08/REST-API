e7b5f5693200b33217f0e0bd43509e38f7c9e5ea
code-reaper08 <v.i.s.h.w.a.08.11.2001@gmail.com>
2021-06-28 23:30:45 +0530

    added routing file strucure

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require('express')
+require('dotenv').config()
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 
-app.get('/',(req,res) => {
-    res.send('Hello there from express')
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser:true},{ useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error',(error) => {
+    console.error(error)
+})
+db.once('open', () => {
+    console.log("Connected to database")
 })
 
-app.get('/movies',(req,res) => {
-    res.send('katradhu Tamizh')
-})
+app.use(express.json())
+
+const subscribersRouter = require("./routes/subscribers")
+app.use('/subscribers', subscribersRouter)
+
+
+
+
+
+
 
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`)
