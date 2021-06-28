c8b3739116e86e50fc83286523aae818bb40d0bd
code-reaper08 <v.i.s.h.w.a.08.11.2001@gmail.com>
2021-06-28 22:59:55 +0530

    routings are made for testing

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/',(req,res) => {
     res.send('Hello there from express')
 })
 
+app.get('/movies',(req,res) => {
+    res.send('katradhu Tamizh')
+})
+
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`)
 })
