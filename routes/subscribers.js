f1805625996b1e3adb499fd80eace9da02b7233f
code-reaper08 <v.i.s.h.w.a.08.11.2001@gmail.com>
2021-06-29 12:04:16 +0530

    Added PATCH request

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -33,8 +33,19 @@ router.post('/', async (req,res) => {
 })
 
 // Update one
-router.patch('/', getSubscriber , (req,res) => {
-
+router.patch('/:id', getSubscriber , async (req,res) => {
+    if (req.body.name != null){
+        res.subscriber.name = req.body.name
+    }
+    if (req.body.subscribedToChannel != null){
+        res.subscriber.subscribedToChannel = req.body.subscribedToChannel
+    }
+    try{
+        const updatedSubscriber = await res.subscriber.save()
+        res.json(updatedSubscriber)
+    }catch(err){
+        res.status(400).json({message : err.message})
+    }
 })
 
 // delete one
